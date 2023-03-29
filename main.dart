46333b86f32404cb97a787be46fd81f20c327e6a
AhmadErshad <116934836+AhmadErshad@users.noreply.github.com>
2023-03-29 10:52:06 -0700

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: Center(child: Text('I am rich')),
+        ),
+        body: Center(
+          child: Text('I am Ahmad Ershad Omari'),
+        ),
+      ),
+    ),
+  );
+}
